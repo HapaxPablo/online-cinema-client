9cfc3d57f365943b6516af99f5fa27881a4669f0
Pavel <81233201+HapaxPablo@users.noreply.github.com>
2023-05-24 04:23:51 +0700

    gfsgdfgdg

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,25 +4,11 @@ import type { AppProps } from 'next/app'
 import { TypeComponentAuthFields } from '@/shared/types/auth.types'
 
 import '../app/assets/styles/globals.scss'
-import { useEffect } from 'react'
-import api from './api'
+
 
 type TypeAppProps = AppProps & TypeComponentAuthFields
 
 const MyApp = ({ Component, pageProps }: TypeAppProps) => {
-
-	useEffect(() => {
-		const fetchData = async () => {
-		  try {
-			const response = await api.get('/some-endpoint'); // Выполняем запрос на сервер Nest.js
-			console.log(response.data);
-		  } catch (error) {
-			console.error(error);
-		  }
-		};
-	
-		fetchData();
-	  }, []);
 
 	return (
 		<MainProvider Component={Component}>
--- pages/api.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'https://cinema-server-1p4o.onrender.com', // Укажите базовый URL вашего сервера API
-});
-
-export default api;
