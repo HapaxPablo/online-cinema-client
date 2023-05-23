5391a087bc4a044ed550dcbf6d893d7ac16f1dcf
Pavel <81233201+HapaxPablo@users.noreply.github.com>
2023-05-24 03:59:46 +0700

    fsdfsffsdf

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
 const nextConfig = {
 	poweredByHeader: false,
 	env: {
-		APP_URL: process.env.REACT_APP_URL,
+		//APP_URL: process.env.REACT_APP_URL,
 		APP_ENV: process.env.REACT_APP_ENV,
-		//APP_SERVER_URL: process.env.REACT_APP_SERVER_URL,
+		APP_URL: process.env.REACT_APP_SERVER_URL,
 	},
 	async rewrites() {
 		return [
