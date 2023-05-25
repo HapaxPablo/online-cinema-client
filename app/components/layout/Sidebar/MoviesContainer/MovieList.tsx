58fdbf862a7311a951cd268863c8e5733065c603
Pavel <81233201+HapaxPablo@users.noreply.github.com>
2023-05-25 15:02:14 +0700

    testMovie

--- app/components/layout/Sidebar/MoviesContainer/MovieList.tsx
+++ app/components/layout/Sidebar/MoviesContainer/MovieList.tsx
@@ -5,17 +5,20 @@ import MovieItem from './MovieItem'
 import styles from './MovieList.module.scss'
 
 const MovieList: FC<IMovieList> = ({ link, title, movies }) => {
+	const limitedMovies = movies.slice(0, 3); // Ограничиваем список фильмов до трех
+  
 	return (
-		<div className={styles.list}>
-			<div className={styles.heading}>{title}</div>
-			{movies.map(movie => (
-				<MovieItem key={movie._id} movie={movie} />
-			))}
-			<Link legacyBehavior href={link}>
-				<a className={styles.button}>Больше</a>
-			</Link>
-		</div>
-	)
-}
-
+	  <div className={styles.list}>
+		<div className={styles.heading}>{title}</div>
+		{limitedMovies.map(movie => (
+		  <MovieItem key={movie._id} movie={movie} />
+		))}
+		{movies.length > 3 && ( // Проверяем, есть ли больше трех фильмов
+		  <Link legacyBehavior href={link}>
+			<a className={styles.button}>Больше</a>
+		  </Link>
+		)}
+	  </div>
+	);
+  };
 export default MovieList
