1ce82071f790a7f11dbb8817e26ebad5553abb01
Pavel <81233201+HapaxPablo@users.noreply.github.com>
2023-06-15 21:44:24 +0700

    Update Menu.tsx

--- app/components/layout/Navigation/MenuContainer/Menu.tsx
+++ app/components/layout/Navigation/MenuContainer/Menu.tsx
@@ -1,26 +1,28 @@
-import dynamic from 'next/dynamic'
-import { FC } from 'react'
-import AuthItems from './auth/AuthItems'
-import { IMenu } from './menu.interface'
-import styles from './Menu.module.scss'
-import MenuItem from './MenuItem'
+import dynamic from 'next/dynamic';
+import { FC } from 'react';
+import AuthItems from './auth/AuthItems';
+import { IMenu } from './menu.interface';
+import styles from './Menu.module.scss';
+import MenuItem from './MenuItem';
 
 const DynamicAuthItems = dynamic(() => import('./auth/AuthItems'), {
 	ssr: false,
-})
+});
 
 const Menu: FC<{ menu: IMenu }> = ({ menu: { items, title } }) => {
+	const AuthItemsComponent = DynamicAuthItems as FC;
+
 	return (
 		<div className={styles.menu}>
 			<div className={styles.heading}>{title}</div>
 			<ul className={styles.ul}>
-				{items.map((item) => (
+				{items.map(item => (
 					<MenuItem key={item.link} item={item} />
 				))}
-				{title === 'Главное' ? <DynamicAuthItems /> : null}
+				{title === 'Главное' ? <AuthItemsComponent /> : null}
 			</ul>
 		</div>
-	)
-}
+	);
+};
 
-export default Menu
+export default Menu;
