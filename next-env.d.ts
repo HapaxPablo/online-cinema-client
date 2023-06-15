53755c6d535c1da1c27c53d91422206fa201ea63
Pavel <pahapasha11@gmail.com>
2023-06-15 16:39:54 +0700

    Update next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 /// <reference types="next/navigation-types/compat/navigation" />
+"declare module 'react-star-rating-component';"
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
