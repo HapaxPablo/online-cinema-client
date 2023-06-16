00f2d14714990805d0e246bad33fb4b5dbb19d7e
Pavel <pahapasha11@gmail.com>
2023-06-17 02:20:30 +0700

    fjnfsfslfhowefwhrgvhoeahgio

--- app/components/ui/MaterialIcon.tsx
+++ app/components/ui/MaterialIcon.tsx
@@ -14,3 +14,6 @@ export const MaterialIcon: FC<{ name: TypeMaterialIconName }> = ({ name }) => {
 		return <IconComponent /> || <MaterialIcons.MdDragIndicator />
 	else return null
 }
+
+//YAZAEBALSA
+//YAEBALTVOEVSO
